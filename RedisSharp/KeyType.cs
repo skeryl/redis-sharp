dd3d8f2e4860d5bc5fcbdf6a0ab86a8210db18b7
Shane <carrollshaned@gmail.com>
2014-07-15 16:32:35 -0500

    Adding to .gitignore
    Adding to .gitignore

--- RedisSharp/KeyType.cs
+++ RedisSharp/KeyType.cs
@@ -1,9 +1,4 @@
-﻿// Type: RedisSharp.KeyType
-// Assembly: RedisSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 1A95670E-3C43-4753-A35D-29B17A12EEC7
-// Assembly location: C:\svn\source\Mdb.SpendAnalysis\trunk\Lib\RedisSharp.dll
-
-namespace RedisSharp
+﻿namespace RedisSharp
 {
   public enum KeyType
   {
